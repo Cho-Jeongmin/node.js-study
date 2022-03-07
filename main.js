e3370894fa9712d37a2b73a23ea366cd33e8d6b8
Cho Jeongmin <97157930+Cho-Jeongmin@users.noreply.github.com>
2022-03-08 03:54:09 +0900

    생활코딩 node.js 31, 32.
    31. 글생성 UI 만들기
    32. POST 방식으로 전송된 데이터 받기

--- main.js
+++ main.js
@@ -1,76 +1,103 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');//url 모듈 요청
+var qs = require('querystring');
+
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB2</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    ${body}
+  </body>
+  </html>
+  `
+}
+
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i++;
+  }
+  list += '</ul>';
+  return list;
+}
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;//   /?id=HTML
-    var queryData = url.parse(_url, true).query;// 객체 { id: 'HTML'}
-    var pathname = url.parse(_url, true).pathname;// queryString을 제외한 pathname.
+  //request는 요청할 때 웹브라우저가 보낸 정보를,
+  //response는 응답할 때 우리가 웹브라우저한테 전송할 정보를 담음
+  var _url = request.url;//   /?id=HTML
+  var queryData = url.parse(_url, true).query;// 객체 { id: 'HTML'}
+  var pathname = url.parse(_url, true).pathname;// queryString을 제외한 pathname.
 
-    if(pathname === '/'){//유효한 주소인 경우.
-      if(queryData.id === undefined){//홈페이지인 경우.
-        fs.readdir('./data', function(error, filelist){//data디렉토리의 파일 리스트 가져오기.
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i++;
-          }
-          list += '</ul>';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+  if(pathname === '/'){//유효한 주소인 경우.
+    if(queryData.id === undefined){//홈페이지인 경우.
+      fs.readdir('./data', function(error, filelist){//data디렉토리의 파일
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {//홈페이지가 아닌 경우
+      fs.readdir('./data', function(error, filelist){//data디렉토리의 파일 리스트 가져오기.
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){//파일 읽기
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         });
-      } else {//홈페이지가 아닌 경우
-        fs.readdir('./data', function(error, filelist){//data디렉토리의 파일 리스트 가져오기.
-          var list = '<ul>';
-          var i = 0;
-          while(i<filelist.length){
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i++;
-          }
-          list += '</ul>';
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){//파일 읽기
-            var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
-            response.writeHead(200);
-            response.end(template);
-          });
-        });
-      }
-    } else {//pathname이 '/'가 아닌 다른 경로로 접속할 경우(유효하지 않은 주소)
-      response.writeHead(404);
-      response.end('Not found');
+      });
     }
+  } else if(pathname === "/create"){//글쓰기 창인 경우
+    fs.readdir('./data', function(error, filelist){//data디렉토리의 파일
+      var title = 'create';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" place holder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `);
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if(pathname === "/create_process"){
+    var body = '';
+    request.on('data', function(data){
+      //'data'는 이벤트
+      //조각조각의 데이터를 서버쪽에서 수신할때마다 이 콜백함수를 호출하기로 약속되어있음. data라는 인자를 통해 수신한 정보를 줌.
+      body = body + data;//콜백이 실행할때마다 data를 추가함.
+    });
+    request.on('end', function(){
+      //'end'는 이벤트임
+      //더이상 들어올 데이터가 없으면 실행되는 콜백함수
+      var post = qs.parse(body);//객체
+      console.log(post);
+      var title = post.title;
+      var description = post.description;
+    });
+    response.writeHead(200);
+    response.end('success');
+  } else {//pathname이 '/'가 아닌 다른 경로로 접속할 경우(유효하지 않은 주소)
+    response.writeHead(404);
+    response.end('Not found');
+  }
 });
 app.listen(3000);
